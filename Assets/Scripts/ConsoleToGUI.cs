e0f49b828592eec3adbad1434e634b0ccef96e1b
HB-Stratos <56313836+HB-Stratos@users.noreply.github.com>
2020-11-29 18:11:38 +0100

    changed key for console toggle

--- Assets/Scripts/ConsoleToGUI.cs
+++ Assets/Scripts/ConsoleToGUI.cs
@@ -8,7 +8,7 @@ public class ConsoleToGUI : MonoBehaviour //Thanks to https://answers.unity.com/
     int kChars = 700;
     void OnEnable() { Application.logMessageReceived += Log; }
     void OnDisable() { Application.logMessageReceived -= Log; }
-    void Update() { if (Input.GetKeyDown(KeyCode.Space)) { doShow = !doShow; } }
+    void Update() { if (Input.GetKeyDown(KeyCode.C)) { doShow = !doShow; } }
     public void Log(string logString, string stackTrace, LogType type)
     {
         // for onscreen...
